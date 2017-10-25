4d3b7ae8ce5131103eb08399471954fcdf430c40
Aniruddha Tekade <atekade1@binghamton.edu>
2017-10-25 16:59:16 -0400

    Linkedin removing duplicates

---
+++ 30_Days_Of_Code/24_More_Linked_Lists/Solution.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+public class Solution {
+    public static HashMap<Integer, Boolean> duplicates = new HashMap<Integer, Boolean>();
+    public static Node removeDuplicates(Node head) {
+        if(head == null) {
+            return head;
+        } else {
+            Node previous = head; 
+            Node p;
+            duplicates.put(previous.data, true);
+            Node current = previous.next;
+            while(current != null) {
+                if(duplicates.containsKey(current.data)) {
+                    p = current;
+                    current = current.next;
+                    previous.next = current;
+                    p.next = null;
+                } else {
+                    duplicates.put(current.data, true);
+                    previous = current;
+                    current = current.next;
+                }
+            } 
+        }
+        return head;
+    }
+
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+
+    public static void display(Node head)
+    {
+        Node start=head;
+        while(start!=null)
+        {
+            System.out.print(start.data+" ");
+            start=start.next;
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Node head=null;
+        int T=sc.nextInt();
+        while(T-->0){
+            int ele=sc.nextInt();
+            head=insert(head,ele);
+        }
+        head=removeDuplicates(head);
+        display(head);
+    }
+}
